6b12231792cb2a42f7db03d1ac6204b68b657c0f
BlackNoxis <steven.darklight@gmail.com>
2015-09-13 06:46:39 +0300

    Deleted old ebuilds, we are keeping them in history

--- app-emacs/csv-mode/files/50csv-mode-gentoo.el
+++
@@ -1,6 +0,0 @@
-
-;;; csv-mode site-lisp configuration 
-
-(add-to-list 'load-path "@SITELISP@")
-(autoload 'csv-mode "csv-mode" "Major mode for editing comma-separated value files." t)
-(add-to-list 'auto-mode-alist '("\\.[Cc][Ss][Vv]\\'" . csv-mode))
